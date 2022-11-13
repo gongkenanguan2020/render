7a7c1c883fd2c9abd5f52613d72b60a925c366d1
gongkenanguan2020 <95908133+gongkenanguan2020@users.noreply.github.com>
2022-11-13 02:36:29 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 #请修改为自己的UUID
 export UUID=470c1981-3bc3-4325-8bc2-9d372f294440
 
-#请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+#请修改为自己设置的伪装站，不要带https://github.com/wulabing/3DCEList/archive/master.zip
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
